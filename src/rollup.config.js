86de0e8cbfae07fc2acd3a9c2d894448b2a49414
EJ0226 <113034820+EJ0226@users.noreply.github.com>
2023-04-17 16:33:21 +0800

    Update rollup.config.js

--- src/rollup.config.js
+++ src/rollup.config.js
@@ -1,3 +1,5 @@
+// rollup.config.js
+
 import vue from 'rollup-plugin-vue';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
@@ -10,27 +12,23 @@ export default {
     format: 'cjs',
   },
   plugins: [
-    resolve({
-      mainFields: ['module', 'main'],
-    }),
-    commonjs({
-      include: /node_modules/,
-      namedExports: {
-        'node_modules/lodash/lodash.js': [
-          'get',
-          'isEmpty',
-          'isEqual',
-          'cloneDeep',
-        ],
-      },
-    }),
-    vue({
-      css: true,
-      compileTemplate: true,
-    }),
+    vue(),
     babel({
       exclude: 'node_modules/**',
       presets: ['@babel/preset-env'],
+    }),
+    commonjs(),
+    resolve({
+      // 在这里指定 axios 模块的路径
+      // 你需要根据你的项目结构修改这个路径
+      // 如果你已经安装了 axios，那么可以试试将路径改为 'node_modules/axios/index.js'
+      mainFields: ['module', 'main', 'browser'],
+      extensions: ['.mjs', '.js', '.json'],
+      preferBuiltins: false,
+      modulesOnly: true,
+      alias: {
+        axios: '/path/to/axios',
+      },
     }),
   ],
 };
