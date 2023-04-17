8df4abc9e106f613f5a9aedce4f6bf6c324ae627
EJ0226 <113034820+EJ0226@users.noreply.github.com>
2023-04-17 16:45:11 +0800

    Add files via upload

--- src/page/Regist.vue
+++ src/page/Regist.vue
@@ -1,80 +1,58 @@
 <template>
   <div class="w-screen h-screen flex flex-col items-center">
     <h1 class="font-black text-[40px] text-center mt-[60px]">帳號註冊</h1>
-
-    <div class="mt-[60px]">
-      <form v-on:submit.prevent="regist">
-        <div>
-          <label for="email">帳號名稱:</label>
-          <br />
+    <form>
+      <div class="mt-[60px]">
+        <label for="email">
           <input
-            minlength="3"
-            maxlength="20"
             type="text"
-            placeholder="請輸入3-20個字符"
+            placeholder="輸入您的email"
             class="w-[240px] h-[40px] bg-[#D9D9D9] text-center mb-[30px]"
             id="email"
           />
-        </div>
-        <div>
-          <label for="password">輸入密碼:</label>
-          <br />
+        </label>
+        <br />
+        <label for="password">
           <input
-            minlength="6"
-            maxlength="16"
             type="password"
-            placeholder="請輸入6-16個字符"
+            placeholder="輸入您的密碼"
             class="w-[240px] h-[40px] bg-[#D9D9D9] text-center mb-[30px]"
             id="“password”"
           />
-        </div>
-        <div>
-          <label for="confirm-password">確認密碼:</label>
-          <br />
+        </label>
+        <br />
+        <label for="confirm-password">
           <input
-            minlength="6"
-            maxlength="16"
-            type="password"
-            placeholder="請再次確認密碼"
+            type="confirm-password"
+            placeholder="確認密碼"
             class="w-[240px] h-[40px] bg-[#D9D9D9] text-center mb-[30px]"
             id="confirm-password"
           />
-        </div>
+        </label>
+      </div>
 
-        <div class="flex justify-center items-center mt-[30px]">
-          <button class="text-[24px] underline" @click="goToLogoin">
-            已有帳號
-          </button>
-          <button
-            type="submit"
-            class="ml-[25px] w-[77px] h-[44px] bg-[#D9D9D9] rounded-[15px] text-[24px] flex justify-center items-center"
-          >
-            註冊
-          </button>
-        </div>
-      </form>
-    </div>
+      <div class="flex justify-center items-center mt-[30px]">
+        <button @click="goToLogin" class="text-[24px] underline">
+          已有帳號
+        </button>
+        <button
+          @click="register"
+          class="ml-[25px] w-[77px] h-[44px] bg-[#D9D9D9] rounded-[15px] text-[24px] flex justify-center items-center"
+        >
+          註冊
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      username: "",
-      email: "",
-      password: "",
-    };
-  },
   methods: {
-    regist() {
-      // 在這裡呼叫後端API進行註冊處理
-      // 如果註冊成功，導航到登入頁面
-      this.$router.push("/login");
-    },
+    register() {},
 
-    goToLogoin() {
-      this.$router.push("/login");
+    goToLogin() {
+      this.$router.push("/UserLogin");
     },
   },
 };
